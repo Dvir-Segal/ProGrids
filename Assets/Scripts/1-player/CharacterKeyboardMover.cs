e7ef201131839dba52e675c3dc567529709a45ee
Dvir-Segal <Dvirsegal96@gmail.com>
2023-05-17 20:39:52 +0300

    uklhj;

--- Assets/Scripts/1-player/CharacterKeyboardMover.cs
+++ Assets/Scripts/1-player/CharacterKeyboardMover.cs
@@ -1,58 +1,4 @@
-﻿// using System.Collections;
-// using UnityEngine;
-// using UnityEngine.InputSystem;
-
-
-// /**
-//  * This component moves a player controlled with a CharacterController using the keyboard.
-//  */
-// [RequireComponent(typeof(CharacterController))]
-// public class CharacterKeyboardMover: MonoBehaviour {
-//     [Tooltip("Speed of player keyboard-movement, in meters/second")]
-//     [SerializeField] float speed = 3.5f;
-//     [SerializeField] float gravity = 9.81f;
-
-//     private CharacterController cc;
-
-//     [SerializeField] InputAction moveAction;
-//     private void OnEnable() { moveAction.Enable(); }
-//     private void OnDisable() { moveAction.Disable(); }
-//     void OnValidate() {
-//         // Provide default bindings for the input actions.
-//         // Based on answer by DMGregory: https://gamedev.stackexchange.com/a/205345/18261
-//         if (moveAction == null)
-//             moveAction = new InputAction(type: InputActionType.Button);
-//         if (moveAction.bindings.Count == 0)
-//             moveAction.AddCompositeBinding("2DVector")
-//                 .With("Up", "<Keyboard>/upArrow")
-//                 .With("Down", "<Keyboard>/downArrow")
-//                 .With("Left", "<Keyboard>/leftArrow")
-//                 .With("Right", "<Keyboard>/rightArrow");
-//     }
-
-//     void Start() {
-//         cc = GetComponent<CharacterController>();
-//     }
-
-//     Vector3 velocity = new Vector3(0,0,0);
-
-//     void Update()  {
-//         if (cc.isGrounded) {
-//             Vector3 movement = moveAction.ReadValue<Vector2>(); // Implicitly convert Vector2 to Vector3, setting z=0.
-//             velocity.x = movement.x * speed;
-//             velocity.z = movement.y * speed;
-//         } else {
-//             velocity.y -= gravity*Time.deltaTime;
-//         }
-
-//         // Move in the direction you look:
-//         velocity = transform.TransformDirection(velocity);
-
-//         cc.Move(velocity * Time.deltaTime);
-//     }
-// }
-
-using System.Collections;
+﻿using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -65,6 +11,7 @@ public class CharacterKeyboardMover: MonoBehaviour {
     [Tooltip("Speed of player keyboard-movement, in meters/second")]
     [SerializeField] float speed = 3.5f;
     [SerializeField] float gravity = 9.81f;
+    [SerializeField] float spForJump = 10f; 
 
     private CharacterController cc;
 
@@ -82,7 +29,8 @@ public class CharacterKeyboardMover: MonoBehaviour {
                 .With("Down", "<Keyboard>/downArrow")
                 .With("Left", "<Keyboard>/leftArrow")
                 .With("Right", "<Keyboard>/rightArrow")
-                .With("Jump", "<Keyboard>/space");
+                .With("Jump", "<Keyboard>/space")
+                .With("Run", "<Keyboard>/shift");
     }
 
     void Start() {
@@ -93,11 +41,20 @@ public class CharacterKeyboardMover: MonoBehaviour {
 
     void Update()  {
         if (cc.isGrounded) {
-            Vector3 movement = moveAction(InputActionType.Button); // Implicitly convert Vector2 to Vector3, setting z=0.
+            Vector3 movement = moveAction.ReadValue<Vector2>(); // Implicitly convert Vector2 to Vector3, setting z=0.
             velocity.x = movement.x * speed;
             velocity.z = movement.y * speed;
         } else {
             velocity.y -= gravity*Time.deltaTime;
+        }
+        if(Input.GetKey(KeyCode.LeftShift)) {
+            Vector3 movement = moveAction.ReadValue<Vector2>();
+            velocity.x = movement.x * speed * 4;
+            velocity.z = movement.y * speed * 4;
+        }
+        if (cc.isGrounded && Input.GetButton("Jump")) {
+            Vector3 movement = moveAction.ReadValue<Vector2>();
+            velocity.y = spForJump;
         }
 
         // Move in the direction you look:
@@ -106,3 +63,4 @@ public class CharacterKeyboardMover: MonoBehaviour {
         cc.Move(velocity * Time.deltaTime);
     }
 }
+
